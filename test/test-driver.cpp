4f17c56cd9e870c1a39ea59d6f9a25b47b56ceeb
Dave Abrahams <dave@boostpro.com>
2013-02-24 09:30:06 -0800

    Create an SVN repository for testing

--- test/test-driver.cpp
+++ test/test-driver.cpp
@@ -6,10 +6,32 @@
 #define BOOST_TEST_MODULE svn2git
 #include <boost/test/unit_test.hpp>
 #include <boost/test/framework.hpp>
+#include <svn_repos.h>
+#include <apr_general.h>
+#include <svn_pools.h>
 
 namespace process = boost::process;
 using namespace boost::process::initializers;
 namespace unit_testing = boost::unit_test::framework;
+
+BOOST_AUTO_TEST_CASE(create_repo)
+  {
+  apr_status_t err = apr_initialize();
+  BOOST_CHECK(err == APR_SUCCESS);
+  
+  svn_repos_t* repos;
+  apr_pool_t* pool = svn_pool_create(0);
+  BOOST_CHECK(pool != 0);
+  
+  svn_repos_create(
+      &repos
+      , "test-repo"
+      , (char const*)0 // unused_1
+      , (char const*)0 // unused_2
+      , (apr_hash_t*)0 // config
+      , (apr_hash_t*)0 // fs_config
+      , pool);
+  }
 
 BOOST_AUTO_TEST_CASE(help)
   {
