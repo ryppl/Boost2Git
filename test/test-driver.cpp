087a0af4116a32ab7756fea626baa8618547bc7c
Dave Abrahams <dave@boostpro.com>
2013-02-24 07:47:40 -0800

    Move test-driver to its own subdirectory
    Also use alternate method (http://stackoverflow.com/a/10824578/125349)
    for updating executables when testing

--- test/test-driver.cpp
+++ test/test-driver.cpp
@@ -1,7 +1,11 @@
 // Copyright Dave Abrahams 2013. Distributed under the Boost
 // Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#include <boost/process.hpp>
+#include <iostream>
+
 int main()
   {
+  std::cout << "ran driver\n";
   return 0;
   }
