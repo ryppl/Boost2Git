838665e36f92fe0e754fbd86e2f7a9c0c35b0a40
Dave Abrahams <dave@boostpro.com>
2013-02-24 09:05:58 -0800

    Use the boost unit test framework

--- test/test-driver.cpp
+++ test/test-driver.cpp
@@ -3,15 +3,18 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/process.hpp>
 #include <iostream>
+#define BOOST_TEST_MODULE svn2git
+#include <boost/test/unit_test.hpp>
+#include <boost/test/framework.hpp>
 
 namespace process = boost::process;
 using namespace boost::process::initializers;
+namespace unit_testing = boost::unit_test::framework;
 
-int main(int argc, char const* argv[])
+BOOST_AUTO_TEST_CASE(help)
   {
   process::execute(
-      run_exe(argv[1]),
+      run_exe(unit_testing::master_test_suite().argv[1]),
       set_cmd_line("svn2git --help")
     );
-  return 0;
   }
