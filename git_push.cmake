1ed2574895d85bd67419134c40c64d93295f3c38
Daniel Pfeifer <daniel@pfeifer-mail.de>
2013-02-25 23:28:26 +0100

    use a script for git push

---
+++ git_push.cmake
@@ -0,0 +1,21 @@
+# Created by Daniel Pfeifer <daniel@pfeifer-mail.de>
+#
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+
+execute_process(COMMAND ${GIT} push --quiet --mirror git@bitbucket.org:boostorg/${NAME}.git)
+execute_process(COMMAND ${GIT} push --quiet --mirror git@bitbucket.org:boostorg/${NAME}.git
+  RESULT_VARIABLE result
+  )
+if(NOT result EQUALS 0)
+  message(FATAL_ERROR "Failed to push ${NAME} to bitbucket!")
+endif()
+
+execute_process(COMMAND ${GIT} push --quiet --mirror git@github.com:boostorg/${NAME}.git)
+execute_process(COMMAND ${GIT} push --quiet --mirror git@github.com:boostorg/${NAME}.git
+  RESULT_VARIABLE result
+  )
+if(NOT result EQUALS 0)
+  message(FATAL_ERROR "Failed to push ${NAME} to github!")
+endif()
