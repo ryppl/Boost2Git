6cdf99cf00c88a18cef0514cdb49ec4edc1034d6
Dave Abrahams <dave@boostpro.com>
2013-06-30 22:04:27 -0700

    PrepareSVNRepo.cmake: correct revision annotations

--- test/PrepareSVNRepo.cmake
+++ test/PrepareSVNRepo.cmake
@@ -19,6 +19,7 @@ endfunction()
 
 # Prepare the basic repository structure
 launch("${SVNADMIN}" create ${REPO_PATH})
+# r1,2,3: create trunk/, tags/, and branches/
 launch(${SVN} mkdir ${LOG_MSG} "create trunk/" ${REPO_URI}/trunk)
 launch(${SVN} mkdir ${LOG_MSG} "create tags/" ${REPO_URI}/tags)
 launch(${SVN} mkdir ${LOG_MSG} "create branches/" ${REPO_URI}/branches)
@@ -31,26 +32,26 @@ function(svn)
   launch(${SVN} ${ARGV} WORKING_DIRECTORY ${WC_PATH})
 endfunction()
 
-# r1: check in a README
+# r4: check in a README
 file(WRITE "${WC_PATH}/README.txt" "This is the README")
 svn(add README.txt)
 svn(commit ${LOG_MSG} "first commit")
 
-# r2: modify README
+# r5: modify README
 file(WRITE "${WC_PATH}/README.txt" "This is the new README")
 svn(commit ${LOG_MSG} "updated README")
 
-# r3,4: create a branch and a tag
+# r6,7: create a branch and a tag
 svn(cp ${LOG_MSG} "create branch" "${REPO_URI}/trunk" "${REPO_URI}/branches/branch1")
 svn(cp ${LOG_MSG} "create tag" "${REPO_URI}/trunk" "${REPO_URI}/tags/tag1")
 
-# r5: commit on the branch
+# r8: commit on the branch
 svn(switch "${REPO_URI}/branches/branch1" .)
 file(WRITE "${WC_PATH}/READYOU.txt" "This is the NEW YOU")
 svn(add READYOU.txt)
 svn(commit ${LOG_MSG} "added READYOU")
 
-# r6: update the trunk
+# r9: update the trunk
 svn(switch "${REPO_URI}/trunk" .)
 file(WRITE "${WC_PATH}/README.txt" "This is the Final README")
 svn(commit ${LOG_MSG} "Final README")
